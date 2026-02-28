a2dae0aa95fa8b600da4315474e3dd1fcaac1659
dhrubomondol66 <dhrubomondol66@gmail.com>
2026-02-28 15:03:39 +0600

    fix missing Link import in profile.jsx to resolve ReferenceError

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import notification from "../assets/notification.png";
 import email from "../assets/email.png";
 import sms from "../assets/chatting.png";
